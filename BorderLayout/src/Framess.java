8ad37297ab26a326f6c8672bc9328fce72064a4d
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-03-16 23:02:18 +0300

    Create Frame class

---
+++ BorderLayout/src/Framess.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Framess {
+    JFrame BorderFrame;
+
+    public Framess(){
+          this.createFrame();
+    }
+
+    public JFrame createFrame(){
+        BorderFrame = new JFrame();
+        BorderFrame.setSize(1000,500);
+        BorderFrame.setTitle("My First Frame");
+        BorderFrame.setVisible(true);
+        return BorderFrame;
+    }
+}
